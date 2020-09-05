44ef5ae401b4b3dd01bb5c50412651368c57f7e4
Tiago Curi <shopper.tiago@gmail.com>
2020-09-05 15:49:59 -0300

    Faltando apenas desafio do csv

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,39 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  UpdateDateColumn,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column('decimal')
   value: number;
 
-  category_id: string;
+  @Column()
+  category_id: string; // vou precisar de reforço aqui. Ver vídeo e ler documentação.
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,67 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string; // Acho que não é isso
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    try {
+      const transactionsRepository = getCustomRepository(
+        TransactionsRepository,
+      );
+
+      const balance = await transactionsRepository.getBalance();
+
+      if (type === 'outcome' && value > balance.total) {
+        throw new AppError(
+          'You cannot make this transaction. Balance is not enough.',
+          400,
+        );
+      }
+      const categoriesRepository = getRepository(Category);
+      let transactionCategory;
+      transactionCategory = await categoriesRepository.findOne({
+        where: {
+          title: category,
+        },
+      });
+
+      if (!transactionCategory) {
+        transactionCategory = categoriesRepository.create({
+          title: category,
+        });
+      }
+
+      await categoriesRepository.save(transactionCategory);
+
+      const transaction = transactionsRepository.create({
+        title,
+        value,
+        type,
+        category_id: transactionCategory.id,
+      });
+
+      await transactionsRepository.save(transaction);
+
+      return transaction;
+    } catch (err) {
+      throw new AppError('Database operation is not concluded.');
+    }
   }
 }
 
